07758b9f7a374fe2b34a387fa50254570115b057
Damiano Ravalico <damianoravalico.uni@gmail.com>
2022-11-07 12:03:00 +0100

    Refactoring

--- src/test/java/dssc/assignment/leapyear/LeapYearTest.java
+++ src/test/java/dssc/assignment/leapyear/LeapYearTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LeapYearTest {
 
     @Test
-    public void isTypicalLeapYear() {
+    public void is1996TypicalLeapYear() {
         LeapYear leapYear = new LeapYear();
         assertEquals(true, leapYear.isLeap(1996));
     }
