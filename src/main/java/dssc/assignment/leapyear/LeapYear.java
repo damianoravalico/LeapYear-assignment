16ed10b7426abc0d7c3036a4cd0bcd8630fd1d35
Damiano Ravalico <damianoravalico.uni@gmail.com>
2022-11-07 17:18:44 +0100

    Added a comment

--- src/main/java/dssc/assignment/leapyear/LeapYear.java
+++ src/main/java/dssc/assignment/leapyear/LeapYear.java
@@ -1,7 +1,7 @@
 package dssc.assignment.leapyear;
 
 public class LeapYear {
-    public static void main(String[] args) {
+    public static void main(String[] args) { // Only tests are performed, main is not used
     }
 
     private LeapYear() {
