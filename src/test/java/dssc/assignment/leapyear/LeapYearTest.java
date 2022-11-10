d6b1d444f566749b8bbce765c0ac53197a185018
Damiano Ravalico <damianoravalico.uni@gmail.com>
2022-11-10 11:13:20 +0100

    Refactoring of tests with @ParameterizedTest

--- src/test/java/dssc/assignment/leapyear/LeapYearTest.java
+++ src/test/java/dssc/assignment/leapyear/LeapYearTest.java
@@ -1,6 +1,5 @@
 package dssc.assignment.leapyear;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -9,40 +8,27 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LeapYearTest {
 
-    @Test
-    public void detect1996AsTypicalLeapYear() {
-        assertTrue(LeapYear.isLeap(1996));
-    }
-
-    @Test
-    public void detect1992AsTypicalLeapYear() {
-        assertTrue(LeapYear.isLeap(1992));
-    }
-
-    @Test
-    public void detect2001AsTypicalCommonYear() {
-        assertFalse(LeapYear.isLeap(2001));
-    }
-
-    @Test
-    public void detect1900AsAtypicalCommonYear() {
-        assertFalse(LeapYear.isLeap(1900));
-    }
-
-    @Test
-    public void detect2000AsAtypicalLeapYear() {
-        assertTrue(LeapYear.isLeap(2000));
-    }
-
     @ParameterizedTest
-    @ValueSource(ints = {1988, 2004, 2008})
+    @ValueSource(ints = {1988, 1992, 1996, 2004, 2008})
     public void detectTypicalLeapYears(int year) {
         assertTrue(LeapYear.isLeap(year));
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1991, 1995, 2003})
+    @ValueSource(ints = {1991, 1995, 2001, 2003, 2005})
     public void detectTypicalCommonYears(int year) {
+        assertFalse(LeapYear.isLeap(year));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1600, 2000})
+    public void detectAtypicalLeapYear(int year) {
+        assertTrue(LeapYear.isLeap(year));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1700, 1800, 1900})
+    public void detectAtypicalCommonYears(int year) {
         assertFalse(LeapYear.isLeap(year));
     }
 
